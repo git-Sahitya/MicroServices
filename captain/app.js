bf889ff342646ef20a067192dbfc3557c3749cb9
git-Sahitya <sahitya5289@gmail.com>
2025-04-25 19:11:46 +0530

    feat: initialize captain service with user authentication routes and server setup
    - Added package.json with necessary dependencies for the project.
    - Created captain.routes.js to handle user registration, login, profile retrieval, logout, and availability toggling.
    - Set up server.js to create and run the Express server on port 3002.

---
+++ captain/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const captainRouter = require('./routes/captain.routes')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const cookieParser = require('cookie-parser')
+
+const connect = require('./db/db')
+connect()
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended :true}))
+app.use(cookieParser())
+
+app.use('/' , captainRouter)
+
+module.exports = app
---
+++ captain/routes/captain.routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const captainController = require("../controller/captain.controller");
+const authMiddleWare = require("../middleware/authMiddleWare");
+
+router.post("/register", captainController.register);
+router.post("/login", captainController.login);
+router.get("/profile", authMiddleWare.captainAuth , captainController.profile);
+router.get("/logout", captainController.logout);
+router.patch('/toggle-availability' , authMiddleWare.captainAuth , captainController.toggleAvailability)
+
+module.exports = router;
